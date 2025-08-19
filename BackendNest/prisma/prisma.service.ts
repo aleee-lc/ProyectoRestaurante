0f442fc816c6b78f9d386a7a4033f0d62fe227b2
aleee-lc <el_alex_2004@hotmail.com>
2025-08-19 10:59:00 -0700

    .

---
+++ BackendNest/prisma/prisma.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    // Cierra conexiones cuando Nest se apaga
+    await this.$disconnect();
+  }
+}
