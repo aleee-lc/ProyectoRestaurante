b1c3ca308bbed9c94c2d67375b59f2efc7c6b4c1
aleee-lc <el_alex_2004@hotmail.com>
2025-11-27 18:17:08 -0700

    .

---
+++ Frontend/FrontRest/src/app/property-select/property-select.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-property-select',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './property-select.component.html',
+  styleUrls: ['./property-select.component.css']
+})
+export class PropertySelectComponent {
+  properties = [
+    { id: 1, name: 'Restaurante', location: 'Primera Planta' },
+    { id: 2, name: 'RestoBar', location: 'Segundo Piso' },
+    { id: 3, name: 'Pool Lounge', location: 'Alberca' },
+  ];
+
+  constructor(private router: Router) {}
+
+  selectProperty(p: any) {
+    localStorage.setItem('selectedProperty', JSON.stringify(p));
+    this.router.navigate(['/home']);
+  }
+}
--- Frontend/FrontRest/src/main.ts
+++ Frontend/FrontRest/src/main.ts
@@ -1,15 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, Routes } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-const routes: Routes = [
-  { path: 'login', loadComponent: () => import('./app/login/login.component').then(m => m.LoginComponent) },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: '**', redirectTo: 'login' },
-];
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideHttpClient()],
-});
 
